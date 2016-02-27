5d799cae29c047234baf90dd5d004a2f83ede335
oobazuaye <oobazuaye@g.hmc.edu>
2016-02-26 17:49:26 -0800

    created __str__ functions for all classes
    all classes can now be printed neatly, and hopefully the last of the empty frame data issues is resolved (an empty space in Lucario's Dash Attack frame data was found).

--- PyScrape.py
+++ PyScrape.py
@@ -36,6 +36,16 @@ class GroundMove:
         self.bkb_wbkb = bkb_wbkb
         self.kbg = kbg
 
+    def __str__(self):
+        return  "      " + self.name + \
+                "\n         Hitbox: " + self.hitbox + \
+                "\n         FAF: " + self.faf + \
+                "\n         Base Damage: " + self.base_dmg + \
+                "\n         Angle: " + self.angle + \
+                "\n         BKB/WBKB: " + self.bkb_wbkb + \
+                "\n         KBG: " + self.kbg + \
+                "\n\n"        
+
 class Aerial:
     def __init__(self, name, hitbox, faf, base_dmg, angle, bkb_wbkb, kbg, landing_lag, autocancel):
         self.name = name
@@ -48,11 +58,29 @@ class Aerial:
         self.landing_lag = landing_lag
         self.autocancel = autocancel
 
+    def __str__(self):
+        return  "      " + self.name + \
+                "\n         Hitbox: " + self.hitbox + \
+                "\n         FAF: " + self.faf + \
+                "\n         Base Damage: " + self.base_dmg + \
+                "\n         Angle: " + self.angle + \
+                "\n         BKB/WBKB: " + self.bkb_wbkb + \
+                "\n         KBG: " + self.kbg + \
+                "\n         Landing Lag: " + self.landing_lag + \
+                "\n         Autocancel: " + self.autocancel + \
+                "\n\n"        
+
 class Grab:
     def __init__(self, name, hitbox, faf):
         self.name = name
         self.hitbox = hitbox
         self.faf = faf
+
+    def __str__(self):
+        return  "      " + self.name + \
+                "\n         Hitbox: " + self.hitbox + \
+                "\n         FAF: " + self.faf + \
+                "\n\n"
 
 class Throw:
     def __init__(self, name, weight, base_dmg, angle, bkb, kbg):
@@ -62,6 +90,15 @@ class Throw:
         self.angle = angle
         self.bkb = bkb
         self.kbg = kbg
+        
+    def __str__(self):
+        return  "      " + self.name + \
+                "\n         Weight Dependent?: " + self.weight + \
+                "\n         Base Damage: " + self.base_dmg + \
+                "\n         Angle: " + self.angle + \
+                "\n         BKB: " + self.bkb + \
+                "\n         KBG: " + self.kbg + \
+                "\n\n"
 
 class Dodge:
     def __init__(self, name, intangibility, faf, notes = "-"):
@@ -69,6 +106,13 @@ class Dodge:
         self.intangibility = intangibility
         self.faf = faf
         self.notes = notes
+
+    def __str__(self):
+        return  "      " + self.name + \
+                "\n         Intangibility: " + self.intangibility + \
+                "\n         FAF: " + self.faf + \
+                "\n         Notes: " + self.notes + \
+                "\n\n"
 
 class Moveset:
     def __init__(self, character_name):
@@ -81,7 +125,43 @@ class Moveset:
         self.grabs = []
         self.throws = []
         self.dodges = []
+
+    def __str__(self):
+        moveset_str = self.character_name + ":\n"
         
+        moveset_str += "   Jabs:\n"
+        for move in self.jabs:
+            moveset_str += str(move)
+
+        moveset_str += "   Dash Attack(s):\n"
+        for move in self.dash_attacks:
+            moveset_str += str(move)
+
+        moveset_str += "   Tilt Attacks:\n"
+        for move in self.tilts:
+            moveset_str += str(move)
+
+        moveset_str += "   Smash Attacks:\n"
+        for move in self.smashes:
+            moveset_str += str(move)
+
+        moveset_str += "   Aerial Attacks:\n"
+        for move in self.aerials:
+            moveset_str += str(move)
+
+        moveset_str += "   Grabs:\n"
+        for move in self.grabs:
+            moveset_str += str(move)
+
+        moveset_str += "   Throws:\n"
+        for move in self.throws:
+            moveset_str += str(move)
+
+        moveset_str += "   Dodges:\n"
+        for move in self.dodges:
+            moveset_str += str(move)
+          
+        return moveset_str
         
     def addMove(self, move):
         if className(move) == "GroundMove":
@@ -150,7 +230,7 @@ def get_moveset(moves):
     counter_to_special_moves = 0
     for move in moves:
         if move not in terms_to_remove:
-            moveset += [move]
+            moveset += [move.encode('utf-8')]
             
         # "Attacks" starts off 3 tables, the 3rd of which is
         # Special Moves...which we don't want.
@@ -216,7 +296,7 @@ def parse_frame_data(character_name, frame_data):
             elif data_len == 7 and isGroundMove(move_name): newMove = GroundMove(*move_data)
             elif data_len == 9 and isAerial(move_name): newMove = Aerial(*move_data)
             else:
-                raise ValueError("ERROR!!!! invalid amount of frame data for" +
+                raise ValueError("ERROR!!!! invalid amount of frame data for " +
                                  move_name +
                                  ". counted " +
                                  str(data_len) +
@@ -246,20 +326,20 @@ def trim_frame_data(raw_frame_data):
         print stripped_data
         print "\n\n\n"
     #The following for loop is for replacing the
-    # empty frame data for some characters' Rapid Jabs
+    # empty frame data for some characters' Rapid Jabs or Dash attacks
     # with '?', so that the amount of data per attack (6 strings)
     # stays consistent
     replace_empty = False
-    rapid_jab_data_counter = 0
+    frame_data_counter = 0
     for datum in stripped_data:
-        if "Rapid Jab" in datum:
+        if isGroundMove(datum):
             replace_empty = True
-            rapid_jab_data_counter = 0
+            frame_data_counter = 0
         elif isMove(datum):
             replace_empty = False
         elif replace_empty:
-            rapid_jab_data_counter += 1
-            if rapid_jab_data_counter > 6:
+            frame_data_counter += 1
+            if frame_data_counter > 6:
                 replace_empty = False
         clean_stripped_data.append("?" if replace_empty and datum == "" else datum)
     new_frame_data = filter(None, clean_stripped_data) #Remove all empty strings
